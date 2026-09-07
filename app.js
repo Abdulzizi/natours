a0e12a15d7b2dd775b2a582ababcb9f369afe13b
Abdulzizi <jawadazizi052@gmail.com>
2026-09-07 10:49:07 +0700

    feat: post feat rough

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require("node:fs");
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3030;
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, "utf8"));
@@ -15,6 +17,29 @@ app.get("/api/v1/tours", (req, res) => {
         result: tours.length,
         data: tours
       });
+})
+
+app.post("/api/v1/tours", (req,res) => {
+    const newId = tours[tours.length - 1].id;
+    const newTour = Object.assign({id: newId},  req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours.json`, JSON.stringify(tours), err => {
+        if (err) {
+            res.status(500)
+                .json({
+                    status: "error",
+                    message: err
+                });
+        }
+
+        res.status(200)
+            .json({
+                status: "success",
+                message: "Data created successfully",
+                data: newTour
+            })
+    })
 })
 
 app.listen(PORT, () => {
