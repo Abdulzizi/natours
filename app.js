ac1fc44784ce474b395ade2d6310c8a26b8197dc
Abdulzizi <jawadazizi052@gmail.com>
2026-09-07 10:12:45 +0700

    feat: initial v1 tours endpoint

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const fs = require("node:fs");
 
 const app = express();
 
 const PORT = 3030;
 
-app.get("/", (req, res) => {
-  res.json({
-    status: "success",
-    message: "Welcome to the app!"
-  }).status(200)
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, "utf8"));
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200)
+      .json({
+        status: "success",
+        message: "Data fetched successfully",
+        result: tours.length,
+        data: tours
+      });
 })
 
 app.listen(PORT, () => {
