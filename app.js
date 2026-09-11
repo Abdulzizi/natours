e0cc9c15a473452179981e59bb8063c5f97be002
Abdulzizi <jawadazizi052@gmail.com>
2026-09-11 08:58:36 +0700

    feat: delete and patch route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("node:fs");
+const crypto = require("crypto");
 
 const app = express();
 
@@ -22,6 +23,14 @@ app.get("/api/v1/tours", (req, res) => {
 app.get("/api/v1/tours/:_id", (req, res) => {
     const tour = tours.find(el => el._id === req.params._id);
 
+    if(!tour){
+        res.status(404)
+            .json({
+                status: "error",
+                message: "Invalid Id",
+            })
+    }
+
     res.status(200)
         .json({
             status: "success",
@@ -31,8 +40,8 @@ app.get("/api/v1/tours/:_id", (req, res) => {
 })
 
 app.post("/api/v1/tours", (req,res) => {
-    const newId = tours[tours.length - 1].id;
-    const newTour = Object.assign({id: newId},  req.body);
+    const newId = crypto.randomBytes(12).toString("hex");
+    const newTour = Object.assign({ _id: newId },  req.body);
 
     tours.push(newTour);
     fs.writeFile(`${__dirname}/dev-data/data/tours.json`, JSON.stringify(tours), err => {
@@ -51,6 +60,45 @@ app.post("/api/v1/tours", (req,res) => {
                 data: newTour
             })
     })
+})
+
+app.patch("/api/v1/tours/:_id", (req, res) => {
+    console.log(req.body);
+    const tour = tours.find(el => el._id === req.params._id);
+
+    if(!tour){
+        res.status(404)
+            .json({
+                status: "error",
+                message: "Invalid Id",
+            })
+    }
+
+    res.status(200)
+        .json({
+            status: "success",
+            message: "Data updated successfully",
+            data: "THIS IS SUCCHED",
+        })
+})
+
+app.delete("/api/v1/tours/:_id", (req, res) => {
+    const tour = tours.find(el => el._id === req.params._id);
+
+    if (!tour){
+        res.status(404)
+            .json({
+                status: "error",
+                message: "Invalid Id",
+            })
+    }
+
+    res.status(200)
+
+        .json({
+            status: "success",
+            message: "Data deleted successfully",
+        })
 })
 
 app.listen(PORT, () => {
